fab74a4e22dc939b07c8e4aa38530a1333e3e51e
Sri Ram Sai Pavan Relangi <107051446+rsrsp21@users.noreply.github.com>
2023-09-27 00:11:16 +0530

    Update 2021_script_cgpa.js

--- 2021_Results/2021_script_cgpa.js
+++ 2021_Results/2021_script_cgpa.js
@@ -436,8 +436,8 @@ table.appendChild(tableBody);
 
         var creditsCell = document.createElement('td');
       var creditsValue = 'NA'; // Default to NA for semesters other than 1-2, 2-1, and 2-2
-    if (key === '1-1') {
-        creditsValue = '19.5' && studentData[0][key] !== '';
+    if (key === '1-1' && studentData[0][key] !== '') {
+        creditsValue = '19.5';
       }
       else if (key === '1-2' && studentData[0][key] !== '') {
         creditsValue = '19.5';
