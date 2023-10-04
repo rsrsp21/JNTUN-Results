26b901d975ff0316e0f7dc12cc38a016a02262ba
Sri Ram Sai Pavan Relangi <107051446+rsrsp21@users.noreply.github.com>
2023-10-05 00:49:30 +0530

    Update 2021_script_cgpa.js

--- 2021_Results/2021_script_cgpa.js
+++ 2021_Results/2021_script_cgpa.js
@@ -476,13 +476,21 @@ function displayResults() {
         messageElement.style.fontWeight = 'bold';
         messageContainer.appendChild(messageElement);
     }
- var supplementaryAppearances = studentData[0]['Supplementary Appearances'];
-
+var supplementaryAppearances = studentData[0]['Supplementary Appearances'];
 var supplementaryContainer = document.getElementById('supplementary-container');
 supplementaryContainer.innerHTML = '';
 
 var supplementaryHeading = document.createElement('h3');
-supplementaryHeading.innerHTML = '<span style="color: blue; font-weight: bold">Supplementary Appearances: </span><span style="color: black; font-weight: bold">' + supplementaryAppearances + '</span>';
+supplementaryHeading.style.color = 'blue';
+supplementaryHeading.style.fontWeight = 'bold';
+supplementaryHeading.textContent = 'Supplementary Appearances: ';
+
+var supplementaryValue = document.createElement('span');
+supplementaryValue.style.color = 'black';
+supplementaryValue.style.fontWeight = 'bold';
+supplementaryValue.textContent = supplementaryAppearances || 'NA';
+
+supplementaryHeading.appendChild(supplementaryValue);
 supplementaryContainer.appendChild(supplementaryHeading);
 }
 
