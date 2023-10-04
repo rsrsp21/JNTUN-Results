37f2eb3f532cf76448688c991be623da2a3c8759
Sri Ram Sai Pavan Relangi <107051446+rsrsp21@users.noreply.github.com>
2023-10-05 00:25:57 +0530

    Update 2021_script_cgpa.js

--- 2021_Results/2021_script_cgpa.js
+++ 2021_Results/2021_script_cgpa.js
@@ -339,151 +339,151 @@ var csvData = `ID,1-1,Credits_1-1,1-2,Credits_1-2,2-1,Credits_2-1,2-2,Credits_2-
 22035A0509,,,,,0.0,20.5,0.0,18.5,0.0,39.0,*
 22035A0510,,,,,0.0,20.5,0.0,18.5,0.0,39.0,*`;
 
-      function parseCSV(csv) {
-            var lines = csv.split('\n');
-            var headers = lines[0].split(',');
+   function parseCSV(csv) {
+    var lines = csv.split('\n');
+    var headers = lines[0].split(',');
 
-            var data = [];
-            for (var i = 1; i < lines.length; i++) {
-                var values = lines[i].split(',');
-                if (values.length === headers.length) {
-                    var entry = {};
-                    for (var j = 0; j < headers.length; j++) {
-                        entry[headers[j].trim()] = values[j].trim();
-                    }
-                    data.push(entry);
-                }
+    var data = [];
+    for (var i = 1; i < lines.length; i++) {
+        var values = lines[i].split(',');
+        if (values.length === headers.length) {
+            var entry = {};
+            for (var j = 0; j < headers.length; j++) {
+                entry[headers[j].trim()] = values[j].trim();
             }
-
-            return data;
+            data.push(entry);
         }
+    }
 
-        function getStudentData(id, data) {
-            var studentData = data.filter(function (entry) {
-                return entry.ID === id;
-            });
+    return data;
+}
 
-            return studentData;
+function getStudentData(id, data) {
+    var studentData = data.filter(function (entry) {
+        return entry.ID === id;
+    });
+
+    return studentData;
+}
+
+var message = ''; // Declare the message variable outside the function
+
+function displayResults() {
+    var studentId = document.getElementById('student-id').value.trim();
+    if (!studentId) {
+        alert('Please enter a valid Roll Number.');
+        return;
+    }
+
+    var studentData = getStudentData(studentId, parseCSV(csvData));
+    if (studentData.length === 0) {
+        alert('No data found for the given Roll Number.');
+        return;
+    }
+
+    var idContainer = document.getElementById('id-container');
+    idContainer.innerHTML = '';
+
+    var idHeading = document.createElement('h3');
+    idHeading.innerHTML = '<span style="color: black; font-weight: bold">Roll Number: </span><span style="color: red; font-weight: bold">' + studentId + '</span>';
+    idContainer.appendChild(idHeading);
+
+    var tableContainer = document.getElementById('table-container');
+    tableContainer.innerHTML = '';
+
+    var table = document.createElement('table');
+    tableContainer.appendChild(table);
+
+    var tableHeader = document.createElement('thead');
+    table.appendChild(tableHeader);
+
+    var headerRow = document.createElement('tr');
+    tableHeader.appendChild(headerRow);
+
+    var semestersHeader = document.createElement('th');
+    semestersHeader.textContent = "Semesters";
+    headerRow.appendChild(semestersHeader);
+
+    var sgpaHeader = document.createElement('th');
+    sgpaHeader.textContent = "SGPA";
+    headerRow.appendChild(sgpaHeader);
+
+    var creditsHeader = document.createElement('th');
+    creditsHeader.textContent = "Credits";
+    headerRow.appendChild(creditsHeader);
+
+    var tableBody = document.createElement('tbody');
+    table.appendChild(tableBody);
+
+    var keys = Object.keys(studentData[0]);
+    keys.forEach(function (key) {
+        if (key !== 'ID' && key !== 'CGPA' && key !== 'Total Credits' && key !== 'Supplementary Appearances') {
+            var row = document.createElement('tr');
+            
+            // Semester label
+            var labelCell = document.createElement('td');
+            labelCell.textContent = key;
+            labelCell.style.fontWeight = 'bold';
+            row.appendChild(labelCell);
+
+            // SGPA
+            var sgpaCell = document.createElement('td');
+            sgpaCell.textContent = studentData[0][key] === '' ? 'NA' : studentData[0][key];
+            row.appendChild(sgpaCell);
+
+            // Find the corresponding Credits key
+            var creditsKey = 'Credits_' + key; // Add 'Credits_' prefix
+            var creditsValue = studentData[0][creditsKey]; // Get the credits value
+
+            // Credits
+            var creditsCell = document.createElement('td');
+            creditsCell.textContent = creditsValue;
+            row.appendChild(creditsCell);
+
+            tableBody.appendChild(row);
         }
+    });
 
-        var message = ''; // Declare the message variable outside the function
+    var cgpaContainer = document.getElementById('cgpa-container');
+    cgpaContainer.innerHTML = '';
 
-        function displayResults() {
-            var studentId = document.getElementById('student-id').value.trim();
-            if (!studentId) {
-                alert('Please enter a valid Roll Number.');
-                return;
-            }
+    var cgpaHeading = document.createElement('h2');
+    cgpaHeading.innerHTML = '<span style="color: black; font-weight: bold">CGPA : </span><span style="color: red; font-weight: bold">' + studentData[0]['CGPA'] + '</span>';
+    cgpaContainer.appendChild(cgpaHeading);
 
-            var studentData = getStudentData(studentId, parseCSV(csvData));
-            if (studentData.length === 0) {
-                alert('No data found for the given Roll Number.');
-                return;
-            }
+    var cgpa = parseFloat(studentData[0]['CGPA']);
+    var supplementaryAppearances = studentData[0]['Supplementary Appearances'];
 
-            var idContainer = document.getElementById('id-container');
-            idContainer.innerHTML = '';
+    if (cgpa >= 7.75 && (supplementaryAppearances === 'NA' || supplementaryAppearances === '')) {
+        message = 'First Class with Distinction';
+    } else if (cgpa >= 6.75) {
+        message = 'First Class';
+    } else if (cgpa >= 5.75) {
+        message = 'Second Class';
+    } else if (cgpa >= 5) {
+        message = 'Pass Class';
+    } else {
+        message = 'Not Applicable';
+    }
 
-            var idHeading = document.createElement('h3');
-            idHeading.innerHTML = '<span style="color: black; font-weight: bold">Roll Number: </span><span style="color: red; font-weight: bold">' + studentId + '</span>';
-            idContainer.appendChild(idHeading);
+    var messageContainer = document.getElementById('message-container');
+    messageContainer.innerHTML = '';
 
-            var tableContainer = document.getElementById('table-container');
-            tableContainer.innerHTML = '';
+    if (message !== '') {
+        var messageElement = document.createElement('h3');
+        messageElement.textContent = message;
+        messageElement.style.color = 'green';
+        messageElement.style.fontWeight = 'bold';
+        messageContainer.appendChild(messageElement);
+    }
+}
 
-            var table = document.createElement('table');
-            tableContainer.appendChild(table);
-
-            var tableHeader = document.createElement('thead');
-            table.appendChild(tableHeader);
-
-            var headerRow = document.createElement('tr');
-            tableHeader.appendChild(headerRow);
-
-            var semestersHeader = document.createElement('th');
-            semestersHeader.textContent = "Semesters";
-            headerRow.appendChild(semestersHeader);
-
-            var sgpaHeader = document.createElement('th');
-            sgpaHeader.textContent = "SGPA";
-            headerRow.appendChild(sgpaHeader);
-
-            var creditsHeader = document.createElement('th');
-            creditsHeader.textContent = "Credits";
-            headerRow.appendChild(creditsHeader);
-
-            var tableBody = document.createElement('tbody');
-            table.appendChild(tableBody);
-
-            var keys = Object.keys(studentData[0]);
-            keys.forEach(function (key) {
-                if (key !== 'ID' && key !== 'CGPA' && key !== 'Total Credits' && key !== 'Supplementary Appearances') {
-                    var row = document.createElement('tr');
-                    
-                    // Semester label
-                    var labelCell = document.createElement('td');
-                    labelCell.textContent = key;
-                    labelCell.style.fontWeight = 'bold';
-                    row.appendChild(labelCell);
-
-                    // SGPA
-                    var sgpaCell = document.createElement('td');
-                    sgpaCell.textContent = studentData[0][key] === '' ? 'NA' : studentData[0][key];
-                    row.appendChild(sgpaCell);
-
-                    // Find the corresponding Credits key
-                    var creditsKey = 'Credits_' + key; // Add 'Credits_' prefix
-                    var creditsValue = studentData[0][creditsKey]; // Get the credits value
-
-                    // Credits
-                    var creditsCell = document.createElement('td');
-                    creditsCell.textContent = creditsValue;
-                    row.appendChild(creditsCell);
-
-                    tableBody.appendChild(row);
-                }
-            });
-
-            var cgpaContainer = document.getElementById('cgpa-container');
-            cgpaContainer.innerHTML = '';
-
-            var cgpaHeading = document.createElement('h2');
-            cgpaHeading.innerHTML = '<span style="color: black; font-weight: bold">CGPA : </span><span style="color: red; font-weight: bold">' + studentData[0]['CGPA'] + '</span>';
-            cgpaContainer.appendChild(cgpaHeading);
-
-            var cgpa = parseFloat(studentData[0]['CGPA']);
-            var supplementaryAppearances = studentData[0]['Supplementary Appearances'];
-
-            if (cgpa >= 7.75 && (supplementaryAppearances === 'NA' || supplementaryAppearances === '')) {
-                message = 'First Class with Distinction';
-            } else if (cgpa >= 6.75) {
-                message = 'First Class';
-            } else if (cgpa >= 5.75) {
-                message = 'Second Class';
-            } else if (cgpa >= 5) {
-                message = 'Pass Class';
-            } else {
-                message = 'Not Applicable';
-            }
-
-            var messageContainer = document.getElementById('message-container');
-            messageContainer.innerHTML = '';
-
-            if (message !== '') {
-                var messageElement = document.createElement('h3');
-                messageElement.textContent = message;
-                messageElement.style.color = 'green';
-                messageElement.style.fontWeight = 'bold';
-                messageContainer.appendChild(messageElement);
-            }
-        }
-
-        function handleKeyPress(event) {
-            if (event.keyCode === 13) {
-                // 13 represents the Enter key
-                displayResults();
-            }
-        }
+function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+        // 13 represents the Enter key
+        displayResults();
+    }
+}
         // Add event listener to input element
         document.getElementById('student-id').addEventListener('keyup', handleKeyPress);
 
